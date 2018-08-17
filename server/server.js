028922b5cdf6f241c3fd47a8d92c162db18ce561
Mauricio Tasca <mauriciotasca@me.com>
2018-08-17 00:42:15 -0300

    Adicionar scaffolding de react e loader de sass

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get('/hello', (req,res)=>{
+    res.send('Hello World');
+});
+
 app.listen(process.env.PORT || 5000, () => console.log('Servidor Gerenciador Financeiro rodando na porta 5000!'));
